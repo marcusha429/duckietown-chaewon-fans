d03182b4b285cd4063ff5e4b0865c299020c5cbf
MartinB47 <mm.bobarshad@gmail.com>
2025-02-23 21:30:30 -0800

    Further modularize making environments

--- utils/env.py
+++ utils/env.py
@@ -1,10 +1,14 @@
 
 from gym_duckietown.simulator import Simulator
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.vec_env.base_vec_env import VecEnv
+# from gym_duckietown.wrappers import *
 from .custom import DuckietownGymnasiumWrapper
 
 RANDOM_SEED = 47
-# Create the environment and apply wrappers
-def make_env():
+
+# Create a raw environment
+def make_raw_env():
     simulator_kwargs = {
         "seed": RANDOM_SEED,
         "map_name": "loop_empty",
@@ -18,6 +22,13 @@ def make_env():
     # Create Duckietown environment
     env = Simulator(**simulator_kwargs)
     print("Initialized environment")
+    return env
+
+
+# Create the environment and apply wrappers
+def make_gym_env() -> VecEnv:
+    # Make a raw environment
+    env = create_raw_env()
 
     # Apply Duckietown wrappers
     # env = ResizeWrapper(env)
@@ -25,10 +36,21 @@ def make_env():
     # env = ImgWrapper(env)
     # env = ActionWrapper(env)
     # env = DtRewardWrapper(env)
-    print("Initialized Duckietown wrappers")
+    # print("Initialized Duckietown wrappers")
 
     # Apply Gymnasium wrapper
     env = DuckietownGymnasiumWrapper(env)
     print("Applied Gymnasium wrapper")
 
+    return env
+
+
+def make_envs(n_envs: int = 4):
+    # Vectorize and parallelize environment
+    env = make_vec_env(
+        env_id=lambda: make_gym_env(),
+        n_envs=n_envs,
+        seed=RANDOM_SEED
+    )
+    print(f"Vectorized and parallelized {n_envs} environments.")
     return env
