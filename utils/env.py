fbb6434b293a9f3c0c2a87f6d436146bb993412a
MartinB47 <mm.bobarshad@gmail.com>
2025-02-24 22:15:09 -0800

    Update environments to accept seed and kwrags

--- utils/env.py
+++ utils/env.py
@@ -1,34 +1,36 @@
 
+from typing import Dict
 from gym_duckietown.simulator import Simulator
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.vec_env.base_vec_env import VecEnv
 # from gym_duckietown.wrappers import *
 from .custom import DuckietownGymnasiumWrapper
 
-RANDOM_SEED = 47
-
 # Create a raw environment
-def make_raw_env():
-    simulator_kwargs = {
-        "seed": RANDOM_SEED,
+def make_raw_env(simulator_kwargs):
+    # Default parameters for the environment
+    default_kwargs = {
         "map_name": "loop_empty",
-        "max_steps": 10,
+        "max_steps": 5000,
         "domain_rand": 0,
-        "camera_width": 640,
-        "camera_height": 480,
         "accept_start_angle_deg": 4,
         "full_transparency": True,
     }
-    # Create Duckietown environment
-    env = Simulator(**simulator_kwargs)
+
+    # If the user provided any kwargs, merge them with the defaults.
+    if simulator_kwargs is not None:
+        default_kwargs.update(simulator_kwargs)
+
+    # Create Duckietown environment using the merged parameters.
+    env = Simulator(**default_kwargs)
     print("Initialized environment")
     return env
 
 
 # Create the environment and apply wrappers
-def make_gym_env() -> VecEnv:
+def make_gym_env(simulator_kwargs) -> VecEnv:
     # Make a raw environment
-    env = make_raw_env()
+    env = make_raw_env(simulator_kwargs)
 
     # Apply Duckietown wrappers
     # env = ResizeWrapper(env)
@@ -45,12 +47,12 @@ def make_gym_env() -> VecEnv:
     return env
 
 
-def make_envs(n_envs: int = 4):
+def make_envs(n_envs: int=4, simulator_kwargs={}, seed: int=47):
     # Vectorize and parallelize environment
     env = make_vec_env(
-        env_id=lambda: make_gym_env(),
+        env_id=lambda: make_gym_env(simulator_kwargs),
         n_envs=n_envs,
-        seed=RANDOM_SEED
+        seed=seed
     )
     print(f"Vectorized and parallelized {n_envs} environments.")
     return env
