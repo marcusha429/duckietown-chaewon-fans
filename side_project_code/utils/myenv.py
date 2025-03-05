9e65f04a90fd5d8140a21b609556b1aaba274951
MartinB47 <mm.bobarshad@gmail.com>
2025-03-05 05:33:35 -0800

    Fix bug where same environment is being created multiple times via seeding

--- side_project_code/utils/myenv.py
+++ side_project_code/utils/myenv.py
@@ -1,6 +1,6 @@
 # utils/env.py
 from gym_duckietown.simulator import Simulator
-from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.vec_env.dummy_vec_env import DummyVecEnv
 from stable_baselines3.common.vec_env.base_vec_env import VecEnv
 
 from gym_duckietown.wrappers import *
@@ -10,10 +10,7 @@ from .mycustom import DuckietownGymnasiumWrapper
 def make_raw_env(simulator_kwargs):
     default_kwargs = {
         "map_name": "loop_empty",
-        "max_steps": 250,
-        "domain_rand": 0,
         "accept_start_angle_deg": 4,
-        "seed": 47,
         "full_transparency": True,
     }
     if simulator_kwargs is not None:
@@ -35,8 +32,11 @@ def make_gym_env(simulator_kwargs) -> VecEnv:
 
 
 def make_envs(n_envs: int = 4, simulator_kwargs={}, seed: int = 47):
-    env = make_vec_env(
-        env_id=lambda: make_gym_env(simulator_kwargs), n_envs=n_envs, seed=seed
-    )
-    print(f"Vectorized and parallelized {n_envs} environments.")
+    env_fns = [
+        lambda i=i: make_gym_env({**simulator_kwargs, "seed": seed + i})
+        for i in range(n_envs)
+    ]
+
+    env = DummyVecEnv(env_fns)
+    print(f"Created {n_envs} environments with unique seeds starting from {seed}.")
     return env
